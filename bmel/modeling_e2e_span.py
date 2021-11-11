fixed logging to report loss after normalizing for number of mentions

bmel/modeling_e2e_span.py
@@ -172,10 +172,10 @@ class DualEncoderBert(BertPreTrainedModel):
                     labels = labels.reshape(-1)  # BN
            
                     linking_loss = self.loss_fn_linker(linker_logits, labels)
-                    logger.info(f"loss: {str(linking_loss.item())}")
                     # Normalize the loss
                     num_mentions = torch.where(labels >= 0)[0].size(0)
                     linking_loss = linking_loss / num_mentions
+                    logger.info(f"loss: {str(linking_loss.item())}")
                 else:
                     linking_loss = None
 