18f56fb9b5ac33867a739f2ddd3758fa4072d07b
coltonflowers1 <coltonflowers@email.arizona.edu>
2021-11-18 16:44:52 -0500

    .

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
   install_requires=['transformers',
                     'torch',
                     'mpi4py',
-                    'git+https://github.com/wikilinks/neleval'
+                    'git',
+                    'https://github.com/wikilinks/neleval'
                     'boto3'
                    ]
 )
