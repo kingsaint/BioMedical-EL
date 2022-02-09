782e8f3c5d5a0e954750ba2f3b121ddfe5c3edfc
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-02-09 09:10:41 -0500

    refactored dataset processors into functions

--- bmel/refactorings/Dataset.py
+++ bmel/refactorings/Dataset.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from lexical_knowledge_base import Lexical_Knowledge_Base
 
 
+
 @dataclass(frozen=True)
 class Mention():
     """Class for keeping track of spans"""
@@ -30,33 +31,21 @@ class Document:
                 for span_2 in doc_spans:
                     if span_1.start_index >= span_2.start_index and span_1.end_index <= span_2.end_index and span_1 != span_2:
                         return True
-    
+    def get_verbose_mentions(self,lkb):
+        #gets mentions along with their concept info
+        mention_dicts = []
+        for mention in self.mentions:
+            sparse_mention_dict = dataclass.as_dict(mention)
+            concept_dict = dataclass.as_dict(lkb.get_concept(mention.concept_id))
+            mention_dicts.append(sparse_mention_dict|concept_dict)
+        return mention_dicts
 
-class EL_Dataset:
-    def __init__(self,docs,lkb):
-        self.docs = docs
-        self.lkb = lkb
-    def check_for_span_overlaps(self):
-        return True in [doc.check_for_overlap() for doc in self.docs]
-    def get_verbose_doc_dictionaries(self):
-        #gets all docs with mentions including their respective concepts, useful for debugging
-        doc_dicts = []
-        for doc in self.docs:
-            mention_dicts = []
-            for mention in doc.mentions:
-                sparse_mention_dict = dataclass.as_dict(mention)
-                concept_dict = dataclass.as_dict(self.lkb.get_concept(mention.concept_id))
-                mention_dicts.append(sparse_mention_dict|concept_dict)
-            doc_dicts.append({"doc_id":doc.doc_id,"doc":doc.message,"mentions":doc.mentions_dic})
-        return doc_dicts
-    
 class Displayer:
     def display(self,el_dataset):
         raise NotImplementedError
 
 class No_Overlap_Displayer(Displayer):
-
-    def display(self,el_dataset):
-        assert not el_dataset.check_for_span_overlaps()
+    def display(self,documents,lkb=None):
+        assert True not in [document.check_for_span_overlaps() for document in documents]
         raise NotImplementedError
         
--- bmel/refactorings/dataset_processors.py
+++ bmel/refactorings/dataset_processors.py
@@ -1,25 +1,44 @@
 from __future__ import annotations
 from collections import defaultdict
-from documents import *
+from Dataset import *
 from dataclasses import dataclass
 
-class Dataset_Processor:
-    def transform(self,el_dataset:EL_Dataset)->EL_Dataset:
-        raise NotImplementedError
+from refactorings.lkb.lexical_knowledge_base import Knowledge_Data
 
-class Overlap_Remover(Dataset_Processor):
-    def transform(self,el_dataset:EL_Dataset)->EL_Dataset:
-        new_docs = [self.remove_overlaps(doc) for doc in el_dataset.docs]
-        return EL_Dataset(new_docs,el_dataset.kb)
-    def remove_overlaps(self,doc:Document)->Document:
-        raise NotImplementedError
+def segment_document(tokenizer,max_mention_per_new_doc,doc:Document) -> Document:
+    def segment_recursive(remaining_text,remaining_mentions,doc_id=0,prev_seq_len=0):
+        omitted_mentions = 0
+        new_document_mentions = []
+        mentions_added = 0 
+        segment_text = ""
+        while mentions_added < len(remaining_mentions):
+            mention_to_add =  remaining_mentions[mentions_added]
+            tentative_segment_text = remaining_text[:mention_to_add.end_index]
+            tokens = tokenizer.tokenize(tentative_segment_text)
+            if (len(new_document_mentions) != max_mention_per_new_doc and len(['[CLS]'] + tokens + ['[SEP]']) < 256):
+                segment_text = tentative_segment_text
+                new_mention_id = doc_id + '_' + str(len(new_document_mentions) + omitted_mentions % max_mention_per_new_doc)
+                mention_start_index_in_new_doc = mention_to_add.start_index - prev_seq_len
+                mention_end_index_in_new_doc = mention_to_add.start_index - prev_seq_len
+                if mention_start_index_in_new_doc < mention_end_index_in_new_doc  and mention_start_index_in_new_doc>= 0 and mention_end_index_in_new_doc > 0:
+                    new_document_mentions.append(Mention(new_mention_id,mention_start_index_in_new_doc,mention_end_index_in_new_doc,mention_to_add.concept_id))
+                else:
+                    omitted_mentions += 1
+                mentions_added += 1
+            else:
+                segmented_doc = Document(doc_id,segment_text,new_document_mentions)
+                remaining_segmented_docs,remaining_omitted_mentions = segment_recursive(remaining_text[len(segment_text):],remaining_mentions[mentions_added:],doc_id+1,prev_seq_len+len(segment_text))
+                return [segmented_doc].extend(remaining_segmented_docs), omitted_mentions + remaining_omitted_mentions
+        segmented_doc = Document(doc_id,remaining_text,new_document_mentions)
+        return [segmented_doc],omitted_mentions#base case
+    return segment_recursive(doc.message,doc.mentions)
 
-class Overlap_Remover_Narrow(Overlap_Remover):
-    def remove_overlaps(self,doc):
-        raise NotImplementedError
 
-class Overlap_Remover_Broad(Overlap_Remover):
-    def remove_overlaps(self,doc):
+def derive_domain_dataset(docs:list[Document],kd:Knowledge_Data) -> tuple(list[Document],Knowledge_Data):
+    pass
+
+def remove_overlaps(doc:Document,broad_strategy:bool=True) -> Document:
+    if broad_strategy:
         old_mentions = doc.mentions
         new_docs = []
         for span_1 in old_mentions:
@@ -30,48 +49,16 @@ class Overlap_Remover_Broad(Overlap_Remover):
             if not contained:
                 new_docs.append(Document(doc.doc_id,doc.message,span_1))
         return new_docs
-        
-class Segmenter(Dataset_Processor):
-    def __init__(self,tokenizer,max_mention_per_new_doc = None):
-        self.tokenizer = tokenizer
-        self.max_mention_per_new_doc = max_mention_per_new_doc
-    def transform(self,el_dataset:EL_Dataset)->EL_Dataset:
-        new_docs = [self.segment(doc) for doc in el_dataset.docs]
-        return EL_Dataset(new_docs,el_dataset.kb)
-    def segment(self,doc:Document) -> Document:
+    else:
         raise NotImplementedError
 
-class Recursive_Segmenter(Segmenter):
-    def segment(self,doc:Document)-> Document:
-        segmented_docs,omitted_mentions = self.segment_recursive(doc.message,doc.mentions)
-        print(f"Omitted_mentions: {omitted_mentions}")
-        return segmented_docs
-    def segment_recursive(self,remaining_text,remaining_mentions,doc_id=0,prev_seq_len=0):
-        omitted_mentions = 0
-        new_document_mentions = []
-        mentions_added = 0 
-        segment_text = ""
-        while mentions_added < len(remaining_mentions):
-            mention_to_add =  remaining_mentions[mentions_added]
-            tentative_segment_text = remaining_text[:mention_to_add.end_index]
-            tokens = self.tokenizer.tokenize(tentative_segment_text)
-            if (len(new_document_mentions) != self.max_mention_per_new_doc and len(['[CLS]'] + tokens + ['[SEP]']) < 256):
-                segment_text = tentative_segment_text
-                new_mention_id = doc_id + '_' + str(len(new_document_mentions) + omitted_mentions % self.max_mention_per_new_doc)
-                mention_start_index_in_new_doc = mention_to_add.start_index - prev_seq_len
-                mention_end_index_in_new_doc = mention_to_add.start_index - prev_seq_len
-                if mention_start_index_in_new_doc < mention_end_index_in_new_doc  and mention_start_index_in_new_doc>= 0 and mention_end_index_in_new_doc > 0:
-                    new_document_mentions.append(Mention(new_mention_id,mention_start_index_in_new_doc,mention_end_index_in_new_doc,mention_to_add.concept_id))
-                else:
-                    omitted_mentions += 1
-                mentions_added += 1
-            else:
-                segmented_doc = Document(doc_id,segment_text,new_document_mentions)
-                remaining_segmented_docs,remaining_omitted_mentions = self.segment(self,remaining_text[len(segment_text):],remaining_mentions[mentions_added:],doc_id+1,prev_seq_len+len(segment_text))
-                return [segmented_doc].extend(remaining_segmented_docs), omitted_mentions + remaining_omitted_mentions
-        segmented_doc = Document(doc_id,remaining_text,new_document_mentions)
-        return [segmented_doc],omitted_mentions#base case
-        
+
+
+
+
+
+
+
 
 
 
