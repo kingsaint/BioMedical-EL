d6451958719044a13991ad9b6654623e17e2ff91
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-02-16 09:09:33 -0500

    .

--- el_toolkit/data_displayer.py
+++ el_toolkit/data_displayer.py
@@ -6,5 +6,7 @@ from el_toolkit.document import Document
 class Displayer():
     def display(doc:Document,lkb:Lexical_Knowledge_Base=None):
         spans = [(mention.start_index,mention.end_index,mention.concept_id) for mention in doc.mentions]
+        if lkb == None:
+            spans = [[spans[0],spans[1],lkb.get_synonyms(spans[2])[0]] for span in spans]
         html = "".join(format_span_line_markup(doc.message, spans))
         return html
